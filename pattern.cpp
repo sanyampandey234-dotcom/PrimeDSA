b8c47424ce8c12df49985911124056803d902640
Sanyam Pandey <sanyampandey234@gmail.com>
2026-02-05 11:17:08 +0530

    added files

---
+++ pattern.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++){\
+        }
+            cout<<" ";
+        }
+        for(int j=1; j<=2*i-1; j++){
+            cout<<i;
+        }
+        cout<<endl;
+        for(int i=1; i<=2i-1; i++){
+            for int 2=i-l+j 2wstreambuf
+            what if it not worl/ATOMIC_WCHAR_T_LOCK_FREE
+
+            return2 ij1
+
+    }
+    return 0;
+} 
+co_return
+& if it get work so the final rresult it  to  programm of else what if 
+ATOMIC_LLONG_LOCK_123:{
+    char8_t
+    int++11122
+    add_lvalue_reference(int i=1+2i)
+    int32_t
+)logical_and for 2nd term true_type
+int64_t (inti=1++2ifstream)
+      ifstream_ int 1_2i*j2i
+      what if the programm is true #
+
+    }
+    ereturn=0{
+        
+    }
